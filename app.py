ed90182b6fce9a75de5320d99901a91d377a463a
MarvhinZavala <mzavmed@gmail.com>
2024-11-27 22:30:10 -0500

    clase 02

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def root():
+    return render_template('index.html')
+
+@app.route("/base", methods=["GET"])
+def base():
+    return render_template('base.html')
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template('home.html')
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template('about.html')
+
+# iniciar servidor
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
